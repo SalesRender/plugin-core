2b1fa1c012fd0e88f4876b8a10e0575f4d08ad91
XAKEPEHOK <icq-475204@ya.ru>
2020-12-08 22:35:43 +0300

    Info need icon

--- src/Actions/InfoAction.php
+++ src/Actions/InfoAction.php
@@ -9,6 +9,7 @@ namespace Leadvertex\Plugin\Core\Actions;
 
 
 use Leadvertex\Plugin\Components\Info\Info;
+use Leadvertex\Plugin\Core\Helpers\PathHelper;
 use Slim\Http\Response;
 use Slim\Http\ServerRequest;
 
@@ -17,6 +18,16 @@ class InfoAction implements ActionInterface
 
     public function __invoke(ServerRequest $request, Response $response, array $args): Response
     {
+        if (!file_exists(PathHelper::getPublic()->down('icon.png'))) {
+            return $response->withJson(
+                [
+                    'code' => 510,
+                    'message' => 'Plugin can not work without 128x128 px "public/icon.png" with transparent background'
+                ],
+                510
+            );
+        }
+
         return $response->withJson(Info::getInstance());
     }
 }
